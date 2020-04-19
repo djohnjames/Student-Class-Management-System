325d986f00344ce7a13fda9924a539d6294de48b
Tyler Sawatzky <32115665+tylersawatzky@users.noreply.github.com>
2020-04-19 14:06:33 -0600

    Added basic SQL setup
    Added MySQL connector to the project and some basic SQL functions to DBManager

--- Milestone III/src/server/DBManager.java
+++ Milestone III/src/server/DBManager.java
@@ -1,16 +1,73 @@
 package server;
 import java.util.ArrayList;
+import java.sql.*;
+
 
 //This class is simulating a database for our
 //program
-public class DBManager {
+public class DBManager implements IDBCredentials {
 	
 	ArrayList <Course> courseList;
 	ArrayList <Student> studentList;
-
+	
 	public DBManager () {
 		courseList = new ArrayList<Course>();
 		studentList = new ArrayList<Student>();
+	}
+	
+	private Connection conn;
+	private ResultSet rs;
+	
+	public void initializeConnection() {
+		try {
+			// Register JDBC driver
+			Driver driver = new com.mysql.cj.jdbc.Driver();
+			DriverManager.registerDriver(driver);
+			// Open a connection
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			System.out.println("Problem");
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			// rs.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void createStudentTable() {
+		String sql = "CREATE TABLE STUDENT " + "(id INTEGER not NULL, " + " name VARCHAR(255), "
+				+ " PRIMARY KEY ( id ))";
+		try {
+			Statement stmt = conn.createStatement(); // construct a statement
+			stmt.executeUpdate(sql); // execute my query (i.e. sql)
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Table can NOT be created!");
+		}
+		System.out.println("Created table in given database...");
+	}
+	
+	public void insertUser(int id, String name) {
+		try {
+			String query = "INSERT INTO STUDENT (ID, name) values(?,?)";
+			PreparedStatement pStat = conn.prepareStatement(query);
+			pStat.setInt(1, id);
+			pStat.setString(2, name);
+			int rowCount = pStat.executeUpdate();
+			System.out.println("row Count = " + rowCount);
+			pStat.close();
+		} catch (SQLException e) {
+			System.out.println("problem inserting user");
+			e.printStackTrace();
+		}
 	}
 
 	public ArrayList readFromDataBase() {
@@ -39,5 +96,14 @@ public class DBManager {
 		
 		return studentList;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
---
+++ Milestone III/src/server/IDBCredentials.java
@@ -0,0 +1,13 @@
+package server;
+
+public interface IDBCredentials {
+	
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/mydb";
+
+	   //  Database credentials
+	   static final String USERNAME = "root";
+	   static final String PASSWORD = "Moshirpour";
+
+}
